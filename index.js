690be1490cf59ff1bc44c1a7e419966687bf914a
Vanessa Wagner <vmwagner@gmail.com>
2022-07-25 17:27:45 -0500

    added search event listener and filter array iteration

--- index.js
+++ index.js
@@ -20,6 +20,7 @@ const api_url = 'https://api.imgflip.com/get_memes'
             console.log(data.data.memes[i])
             memeCurrent = data.data.memes[i]
             image.src = memeCurrent.url
+            
         
         })
         
@@ -29,13 +30,45 @@ const api_url = 'https://api.imgflip.com/get_memes'
             console.log(data.data.memes[i])
             memeCurrent = data.data.memes[i]
             image.src = memeCurrent.url
-
-
+            console.log(memeCurrent.name)
+            title = memeCurrent.name
     })
 
-        
 
+    const search = document.getElementById("search");
+    const results = document.getElementById("results");
+    //define input in search field
+    let search_term = "";
+    
+    
+    const listMemes = () => {
+    //clear list
+      results.innerHTML = "";
+      //loop over data elements to filter if meme NAME matches search text
+      data.data.memes.filter((item) => {
+          return (
+            item.name.toLowerCase().includes(search_term) 
+          );
+        })
+        .forEach((e) => {
+            //append a new list item to ul
+          const li = document.createElement("li");
+          li.innerHTML = `<b>Name:</b> ${e.name}  || <b>url:</b> ${e.url}`;
+          results.appendChild(li);
+        });
+    };
+    
+    listMemes();
+
+    //capture the search input
+    search.addEventListener("input", (event) => {
+      search_term = event.target.value.toLowerCase();
+      listMemes();
+    });
 }
+
 
     getMEME();
 
+    
+    
